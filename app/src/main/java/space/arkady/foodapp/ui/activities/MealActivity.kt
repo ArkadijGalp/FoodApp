a63b8e86a3ee841ca65802cde55778e7958d7c6f
Аркадий <arkady.galperin@gmail.com>
2022-07-22 21:02:09 +0300

    meal activity addon

---
+++ app/src/main/java/space/arkady/foodapp/ui/activities/MealActivity.kt
@@ -0,0 +1,12 @@
+package space.arkady.foodapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import space.arkady.foodapp.R
+
+class MealActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal)
+    }
+}
--- app/src/main/java/space/arkady/foodapp/ui/fragments/HomeFragment.kt
+++ app/src/main/java/space/arkady/foodapp/ui/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package space.arkady.foodapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,8 @@ import com.bumptech.glide.Glide
 import space.arkady.foodapp.R
 import space.arkady.foodapp.databinding.FragmentHomeBinding
 import space.arkady.foodapp.models.Meal
+import space.arkady.foodapp.ui.activities.MainActivity
+import space.arkady.foodapp.ui.activities.MealActivity
 import space.arkady.foodapp.ui.viewmodels.HomeViewModel
 
 
@@ -40,14 +43,22 @@ class HomeFragment : Fragment() {
 
         homeMvvm.getRandomMeal()
         observerRandomMeal()
+        onRandomMealClickListener()
 
+    }
+
+    private fun onRandomMealClickListener() {
+        binding.randomMealCard.setOnClickListener {
+            val intent = Intent(activity, MealActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun observerRandomMeal() {
         homeMvvm.observeRandomMealLiveData().observe(viewLifecycleOwner, object : Observer<Meal> {
             override fun onChanged(t: Meal?) {
                 Glide.with(this@HomeFragment)
-                    .load(t!!.strMealThumb).into(binding.imageRandomMeal)
+                    .load(t?.strMealThumb).into(binding.imageRandomMeal)
             }
 
         })
